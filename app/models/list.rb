636b0a2574a680dfda6bb6bef5b0c50829ad6049
yan monteiro <yanlinhares9@hotmail.com>
2023-12-12 13:55:42 -0300

    MVC

---
+++ app/models/list.rb
@@ -0,0 +1,9 @@
+class List < ApplicationRecord
+  has_many :bookmarks, dependent: :destroy
+  has_many :movies, through: :bookmarks, dependent: :destroy
+  has_many :reviews, dependent: :destroy
+  has_one_attached :photo
+
+  validates :photo, presence: true
+  validates :name, presence: true, uniqueness: true
+end
